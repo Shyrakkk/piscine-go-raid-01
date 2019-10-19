fe50a4de6e3ea0240b7df59279c3ca5b350487bf
Bekassyl <student@ALEM-316edb.alem.school>
2019-10-19 15:13:05 +0600

    b

--- raid1b.go
+++ raid1b.go
@@ -2,7 +2,28 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func Raid1b(x, y int) {
+func PrintWidth2(x int, last int) {
+	for i := 0; i < x; i++ {
+		if i == 0 {
+			if last == 1 {
+				z01.PrintRune(92)
+				continue
+			}
+			z01.PrintRune('/')
+		} else if i == x-1 {
+			if last == 1 {
+				z01.PrintRune('/')
+				continue
+			}
+			z01.PrintRune(92)
+		} else {
+			z01.PrintRune('*')
+		}
+	}
+	z01.PrintRune(10)
+}
+
+func Raid1a(x, y int) {
 
 	if x == 0 && y == 0 {
 		return
@@ -16,12 +37,12 @@ func Raid1b(x, y int) {
 		return
 	}
 
-	PrintWidth(x, 0)
+	PrintWidth2(x, 0)
 
 	if y > 1 {
 		for i2 := 1; i2 < y; i2++ {
 			if i2 == y-1 {
-				PrintWidth(x, 1)
+				PrintWidth2(x, 1)
 				break
 			} else if x > 1 {
 				z01.PrintRune('*')
